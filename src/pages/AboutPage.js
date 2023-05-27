d678aba9c15c923679aa9cfa99d9eea38f079495
Nguyen Van Giang <giangnvph26948@fpt.edu.vn>
2023-05-27 15:34:04 +0700

    update-2

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,9 @@
-import Header from "../components/Header";
-import Banner from "../components/Banner";
-import Footer from "../components/Footer";
+
 
 const AboutPage = () => {
     return `
         <div class="container max-w-4xl mx-auto px-4">
-            hahaahaahaahaahaahaahaahaaha
+            hahahahahahahaahaahaahaahaahaahaahaahaahaahaaha
         </div>
     `;
 };
